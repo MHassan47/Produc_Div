5e2264acc90df44820668c35fbd8d9a92e2e3de3
Mohamed <mhassankd35@gmail.com>
2022-07-07 17:03:07 -0500

    seed users data

---
+++ server/db/seeds/01_users.sql
@@ -0,0 +1,7 @@
+INSERT INTO users (first_name, last_name, email, password, photo_url, role) VALUES ('John', 'Smith', 'johnsmith@gmail.com', '$2a$10$ZSN27SEQSBgC112fygR9P.Sdtlk7tub6gNaYqDiKRk/kw2tmh/Hwe', 'https://robohash.org/cumsintsed.png?size=50x50&set=set1', 'Front End Developer');
+INSERT INTO users (first_name, last_name, email, password, photo_url, role) VALUES ('Jane', 'Doe', 'janedoe@gmail.com', '$2a$10$ZSN27SEQSBgC112fygR9P.Sdtlk7tub6gNaYqDiKRk/kw2tmh/Hwe', 'https://robohash.org/illodeseruntcum.png?size=50x50&set=set1', 'Full Stack Developer');
+INSERT INTO users (first_name, last_name, email, password, photo_url, role) VALUES('Joe', 'Scott', 'joescott@gmail.com', '$2a$10$ZSN27SEQSBgC112fygR9P.Sdtlk7tub6gNaYqDiKRk/kw2tmh/Hwe', 'https://robohash.org/quasaliquidfugiat.png?size=50x50&set=set1', 'Back End Developer');
+
+
+
+
