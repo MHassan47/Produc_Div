201d16219f0f6e6b8e29d82b13467b1f1c68696e
Mohamed <mhassankd35@gmail.com>
2022-07-07 22:49:00 -0500

    fix user route logic for GET all users

--- server/routes/usersRoutes.js
+++ server/routes/usersRoutes.js
@@ -1,12 +1,17 @@
 const express = require("express");
 const router = express.Router();
 
+// Get all users
 module.exports = (db) => {
   router.get("/", (req, res) => {
     db.query(`SELECT * FROM users;`)
-      .then((data) => {
-        const users = data.rows;
-        res.json({ users });
+      .then(({ rows: users }) => {
+        res.json(
+          users.reduce(
+            (previous, current) => ({ ...previous, [current.id]: current }),
+            {}
+          )
+        );
       })
       .catch((err) => {
         res.status(500).json({ error: err.message });
