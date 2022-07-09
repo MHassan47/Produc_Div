90c0acf011517967a0b9a9dd51f73936bd980c4a
Mohamed <mhassankd35@gmail.com>
2022-07-09 00:53:48 -0500

    changes categories table name to columns

--- server/db/schema/02_tasks.sql
+++ server/db/schema/02_tasks.sql
@@ -1,8 +1,8 @@
-DROP TABLE IF EXISTS categories CASCADE;
+DROP TABLE IF EXISTS columns CASCADE;
 DROP TABLE IF EXISTS tasks CASCADE;
 DROP TABLE IF EXISTS users_to_tasks CASCADE;
 
-CREATE TABLE categories (
+CREATE TABLE columns (
     id SERIAL PRIMARY KEY NOT NULL,
     title VARCHAR(255) NOT NULL
 );
@@ -13,7 +13,7 @@ CREATE TABLE tasks (
     name VARCHAR(255) NOT NULL,
     created_at TIMESTAMP NOT NULL DEFAULT NOW(),
     owner_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
-    category_id INTEGER REFERENCES categories(id) ON DELETE CASCADE
+    column_id INTEGER REFERENCES columns(id) ON DELETE CASCADE
 );
 
 CREATE TABLE users_to_tasks (
--- server/db/seeds/02_tasks.sql
+++ server/db/seeds/02_tasks.sql
@@ -1,13 +1,13 @@
-INSERT INTO categories (title) VALUES ('To Do');
-INSERT INTO categories (title) VALUES ('In Progress');
-INSERT INTO categories (title) VALUES ('Complete');
+INSERT INTO columns (title) VALUES ('To Do');
+INSERT INTO columns (title) VALUES ('In Progress');
+INSERT INTO columns (title) VALUES ('Complete');
 
-INSERT INTO tasks ( name, created_at, owner_id, category_id ) VALUES ('Layout Design', '2022-07-07', 1, 1);
-INSERT INTO tasks ( name, created_at, owner_id, category_id ) VALUES ('Database Design', '2022-07-08', 1, 1);
-INSERT INTO tasks ( name, created_at, owner_id, category_id ) VALUES ('Create Schedule', '2022-07-06', 2, 2);
-INSERT INTO tasks ( name, created_at, owner_id, category_id ) VALUES ('Choose tech stack', '2022-07-06', 3, 2);
-INSERT INTO tasks ( name, created_at, owner_id, category_id ) VALUES ('Create Schedule', '2022-07-06', 2, 3 );
-INSERT INTO tasks ( name, created_at, owner_id, category_id ) VALUES ('Choose Idea', '2022-07-05', 3, 3);
+INSERT INTO tasks ( name, created_at, owner_id, column_id ) VALUES ('Layout Design', '2022-07-07', 1, 1);
+INSERT INTO tasks ( name, created_at, owner_id, column_id ) VALUES ('Database Design', '2022-07-08', 1, 1);
+INSERT INTO tasks ( name, created_at, owner_id, column_id ) VALUES ('Create Schedule', '2022-07-06', 2, 2);
+INSERT INTO tasks ( name, created_at, owner_id, column_id ) VALUES ('Choose tech stack', '2022-07-06', 3, 2);
+INSERT INTO tasks ( name, created_at, owner_id, column_id ) VALUES ('Create Schedule', '2022-07-06', 2, 3 );
+INSERT INTO tasks ( name, created_at, owner_id, column_id ) VALUES ('Choose Idea', '2022-07-05', 3, 3);
 
 INSERT INTO users_to_tasks (user_id, task_id) VALUES (1, 1);
 INSERT INTO users_to_tasks (user_id, task_id) VALUES (2, 1);
