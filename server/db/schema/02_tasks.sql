04a2ef36ad58514d323be43fb47940d790832477
Mohamed <mhassankd35@gmail.com>
2022-07-09 14:36:22 -0500

    add projects table and seeds

--- server/db/schema/02_tasks.sql
+++ server/db/schema/02_tasks.sql
@@ -4,7 +4,8 @@ DROP TABLE IF EXISTS users_to_tasks CASCADE;
 
 CREATE TABLE columns (
     id SERIAL PRIMARY KEY NOT NULL,
-    title VARCHAR(255) NOT NULL
+    title VARCHAR(255) NOT NULL,
+    project_id INTEGER REFERENCES projects(id) ON DELETE CASCADE
 );
 
 
@@ -13,7 +14,8 @@ CREATE TABLE tasks (
     name VARCHAR(255) NOT NULL,
     created_at TIMESTAMP NOT NULL DEFAULT NOW(),
     owner_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
-    column_id INTEGER REFERENCES columns(id) ON DELETE CASCADE
+    column_id INTEGER REFERENCES columns(id) ON DELETE CASCADE,
+    project_id INTEGER REFERENCES projects(id) ON DELETE CASCADE
 );
 
 CREATE TABLE users_to_tasks (
---
+++ server/db/schema/03_projects.sql
@@ -0,0 +1,13 @@
+DROP TABLE IF EXISTS projects CASCADE;
+
+CREATE TABLE projects (
+    id SERIAL PRIMARY KEY NOT NULL,
+    name VARCHAR(255) NOT NULL,
+    owner_id INTEGER REFERENCES users(id) ON DELETE CASCADE
+);
+
+CREATE TABLE users_to_projects (
+    id SERIAL PRIMARY KEY NOT NULL,
+    user_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
+    project_id INTEGER REFERENCES projects(id) ON DELETE CASCADE  
+);
