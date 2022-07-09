610fd0e6ea00e1ef370e4dec5f0d889be66b26f5
Mohamed <mhassankd35@gmail.com>
2022-07-09 17:08:06 -0500

    removes columns table + restructures task data

--- server/db/seeds/02_tasks.sql
+++ server/db/seeds/02_tasks.sql
@@ -1,26 +1,17 @@
-INSERT INTO columns (title, cproject_id) VALUES ('To Do', 1);
-INSERT INTO columns (title, cproject_id) VALUES ('In Progress', 1);
-INSERT INTO columns (title, cproject_id) VALUES ('Complete', 1);
 
-INSERT INTO columns (title, cproject_id) VALUES ('To Do', 2);
-INSERT INTO columns (title, cproject_id) VALUES ('In Progress', 2);
-INSERT INTO columns (title, cproject_id) VALUES ('Complete', 2);
+INSERT INTO tasks ( name, col, created_at, owner_id, project_id ) VALUES ('Layout Design', 'To Do', '2022-07-07', 1, 1);
+INSERT INTO tasks ( name, col, created_at, owner_id, project_id ) VALUES ('Database Design', 'To Do', '2022-07-08', 1, 1);
+INSERT INTO tasks ( name, col, created_at, owner_id, project_id ) VALUES ('Create Schedule', 'In Progress', '2022-07-06', 2, 1);
+INSERT INTO tasks ( name, col, created_at, owner_id, project_id ) VALUES ('Choose tech stack', 'In Progress', '2022-07-06', 3, 1);
+INSERT INTO tasks ( name, col, created_at, owner_id, project_id ) VALUES ('Create Schedule', 'Complete', '2022-07-06', 2, 1);
+INSERT INTO tasks ( name, col, created_at, owner_id, project_id ) VALUES ('Choose Idea', 'Complete', '2022-07-05', 3, 1);
 
-
-
-INSERT INTO tasks ( name, created_at, owner_id, column_id ) VALUES ('Layout Design', '2022-07-07', 1, 1);
-INSERT INTO tasks ( name, created_at, owner_id, column_id ) VALUES ('Database Design', '2022-07-08', 1, 1);
-INSERT INTO tasks ( name, created_at, owner_id, column_id ) VALUES ('Create Schedule', '2022-07-06', 2, 2);
-INSERT INTO tasks ( name, created_at, owner_id, column_id ) VALUES ('Choose tech stack', '2022-07-06', 3, 2);
-INSERT INTO tasks ( name, created_at, owner_id, column_id ) VALUES ('Create Schedule', '2022-07-06', 2, 3 );
-INSERT INTO tasks ( name, created_at, owner_id, column_id ) VALUES ('Choose Idea', '2022-07-05', 3, 3);
-
-INSERT INTO tasks ( name, created_at, owner_id, column_id ) VALUES ('Layout Design', '2022-07-07', 1, 4);
-INSERT INTO tasks ( name, created_at, owner_id, column_id ) VALUES ('Database Design', '2022-07-08', 1, 4);
-INSERT INTO tasks ( name, created_at, owner_id, column_id ) VALUES ('Create Schedule', '2022-07-06', 2, 5);
-INSERT INTO tasks ( name, created_at, owner_id, column_id ) VALUES ('Choose tech stack', '2022-07-06', 3, 5);
-INSERT INTO tasks ( name, created_at, owner_id, column_id ) VALUES ('Create Schedule', '2022-07-06', 2, 6 );
-INSERT INTO tasks ( name, created_at, owner_id, column_id ) VALUES ('Choose Idea', '2022-07-05', 3, 6);
+INSERT INTO tasks ( name, col, created_at, owner_id, project_id ) VALUES ('Layout Design', 'To Do', '2022-07-07', 1, 2);
+INSERT INTO tasks ( name, col, created_at, owner_id, project_id ) VALUES ('Database Design', 'To Do', '2022-07-08', 1, 2);
+INSERT INTO tasks ( name, col, created_at, owner_id, project_id ) VALUES ('Create Schedule', 'In Progress', '2022-07-06', 2, 2);
+INSERT INTO tasks ( name, col, created_at, owner_id, project_id ) VALUES ('Choose tech stack', 'In Progress', '2022-07-06', 3, 2);
+INSERT INTO tasks ( name, col, created_at, owner_id, project_id ) VALUES ('Create Schedule', 'Complete', '2022-07-06', 2, 2);
+INSERT INTO tasks ( name, col, created_at, owner_id, project_id ) VALUES ('Choose Idea', 'Complete', '2022-07-05', 3, 2);
 
 INSERT INTO users_to_tasks (user_id, task_id) VALUES (1, 1);
 INSERT INTO users_to_tasks (user_id, task_id) VALUES (2, 1);
