7bd71d058af6ae1045a02e4cbe159b970034a04e
Mohamed <mhassankd35@gmail.com>
2022-07-10 13:38:14 -0500

    create prototype kanban and cards

---
+++ client/src/components/Kanban/Kanban.js
@@ -0,0 +1,86 @@
+import { DragDropContext, Draggable, Droppable } from "react-beautiful-dnd";
+import { useState } from "react";
+import Card from "../Task/Card";
+import "./Kanban.css";
+
+// const test = [
+//   { id: 1, name: "one" },
+//   { id: 2, name: "two" },
+// ];
+
+const Kanban = (props) => {
+  const state = props.state;
+  const setState = props.setState;
+  const [column, setColumn] = useState(["To Do", "In Progress", "Complete"]);
+
+  const onDragEnd = (result) => {
+    // console.log("-------iiii", result);
+    const destination = result.destination;
+    const sourceIndex = result.source.index;
+    const col = result.destination.droppableId;
+    const task_id = Number(result.draggableId);
+    const items = state.tasks.map((task) => {
+      if (task.id === task_id) {
+        return { ...task, col };
+      } else {
+        return task;
+      }
+    });
+    // console.log("??????", items);
+    // const [reorderedItem] = items.splice(result.source.index, 1);
+    // items.splice(result.destination.index, 0, reorderedItem);
+    setState({ ...state, tasks: items });
+
+    console.log("dragged item");
+  };
+  // console.log("UPDATED STATE", state);
+
+  return (
+    <DragDropContext onDragEnd={onDragEnd}>
+      <div className="kanban">
+        {column.map((element) => (
+          <Droppable key={element} droppableId={element}>
+            {(provided) => (
+              <div
+                {...provided.droppableProps}
+                className="kanban__section"
+                ref={provided.innerRef}
+              >
+                <div className="kanban__section__title">{element}</div>
+                <div className="kanban__section__content">
+                  {state.tasks.map((task, index) => {
+                    if (task.col === element && task.project_id === 1)
+                      return (
+                        <Draggable
+                          key={task.id}
+                          draggableId={task.id.toString()}
+                          index={index}
+                        >
+                          {(provided, snapshot) => (
+                            <div
+                              ref={provided.innerRef}
+                              {...provided.draggableProps}
+                              {...provided.dragHandleProps}
+                              style={{
+                                ...provided.draggableProps.style,
+                                opacity: snapshot.isDragging ? "0.5" : "1",
+                              }}
+                            >
+                              <Card>{task.name}</Card>
+                            </div>
+                          )}
+                        </Draggable>
+                      );
+                  })}
+                  {provided.placeholder}
+                </div>
+              </div>
+            )}
+          </Droppable>
+        ))}
+      </div>
+    </DragDropContext>
+  );
+};
+
+export default Kanban;
