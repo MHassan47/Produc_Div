e5fbaf8bc14b5af1af99d1e0da0a1db6d9a94aaa
Mohamed <mhassankd35@gmail.com>
2022-07-10 13:19:51 -0500

    imports state and passes to kanban component

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,14 @@
+import { useState } from "react";
 import "./App.css";
+import Kanban from "./components//Kanban/Kanban";
+import useApplicationData from "./hooks/useApplicationData";
 
 function App() {
+  const { state, setState } = useApplicationData();
   return (
     <div className="App">
       <div>Hello World</div>
+      <Kanban state={state} setState={setState} />
     </div>
   );
 }
