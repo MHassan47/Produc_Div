35a995ffe22493bad70f010ad07187cebaa5420c
Mohamed <mhassankd35@gmail.com>
2022-07-10 17:20:31 -0500

    adds axios put request to move task between columns

--- client/src/components/Kanban/Kanban.js
+++ client/src/components/Kanban/Kanban.js
@@ -1,5 +1,6 @@
 import { DragDropContext, Draggable, Droppable } from "react-beautiful-dnd";
 import { useState } from "react";
+import axios from "axios";
 import Card from "../Task/Card";
 import "./Kanban.css";
 
@@ -12,13 +13,16 @@ const Kanban = (props) => {
   const state = props.state;
   const setState = props.setState;
   const [column, setColumn] = useState(["To Do", "In Progress", "Complete"]);
+  // const [project, setProject] = useState(1);
 
   const onDragEnd = (result) => {
-    // console.log("-------iiii", result);
+    console.log("-------iiii", result);
     const destination = result.destination;
     const sourceIndex = result.source.index;
     const col = result.destination.droppableId;
+    console.log(typeof col);
     const task_id = Number(result.draggableId);
+
     const items = state.tasks.map((task) => {
       if (task.id === task_id) {
         return { ...task, col };
@@ -29,7 +33,10 @@ const Kanban = (props) => {
     // console.log("??????", items);
     // const [reorderedItem] = items.splice(result.source.index, 1);
     // items.splice(result.destination.index, 0, reorderedItem);
-    setState({ ...state, tasks: items });
+    return axios
+      .put(`http://localhost:8080/api/tasks/move/${task_id}`, { col: col })
+      .then(setState({ ...state, tasks: items }))
+      .catch((error) => console.log(error));
 
     console.log("dragged item");
   };
@@ -52,7 +59,7 @@ const Kanban = (props) => {
                     if (task.col === element && task.project_id === 1)
                       return (
                         <Draggable
-                          key={task.id}
+                          key={task.id.toString()}
                           draggableId={task.id.toString()}
                           index={index}
                         >
