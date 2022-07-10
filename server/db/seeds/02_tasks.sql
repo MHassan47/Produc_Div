a8632290455439d58bd59245e5c5c844738364de
Mohamed <mhassankd35@gmail.com>
2022-07-10 13:36:53 -0500

    small adjustment to mock data

--- server/db/seeds/02_tasks.sql
+++ server/db/seeds/02_tasks.sql
@@ -1,14 +1,14 @@
 
 INSERT INTO tasks ( name, col, created_at, owner_id, project_id ) VALUES ('Layout Design', 'To Do', '2022-07-07', 1, 1);
 INSERT INTO tasks ( name, col, created_at, owner_id, project_id ) VALUES ('Database Design', 'To Do', '2022-07-08', 1, 1);
-INSERT INTO tasks ( name, col, created_at, owner_id, project_id ) VALUES ('Create Schedule', 'In Progress', '2022-07-06', 2, 1);
+INSERT INTO tasks ( name, col, created_at, owner_id, project_id ) VALUES ('Create kanban', 'In Progress', '2022-07-06', 2, 1);
 INSERT INTO tasks ( name, col, created_at, owner_id, project_id ) VALUES ('Choose tech stack', 'In Progress', '2022-07-06', 3, 1);
 INSERT INTO tasks ( name, col, created_at, owner_id, project_id ) VALUES ('Create Schedule', 'Complete', '2022-07-06', 2, 1);
 INSERT INTO tasks ( name, col, created_at, owner_id, project_id ) VALUES ('Choose Idea', 'Complete', '2022-07-05', 3, 1);
 
 INSERT INTO tasks ( name, col, created_at, owner_id, project_id ) VALUES ('Layout Design', 'To Do', '2022-07-07', 1, 2);
 INSERT INTO tasks ( name, col, created_at, owner_id, project_id ) VALUES ('Database Design', 'To Do', '2022-07-08', 1, 2);
-INSERT INTO tasks ( name, col, created_at, owner_id, project_id ) VALUES ('Create Schedule', 'In Progress', '2022-07-06', 2, 2);
+INSERT INTO tasks ( name, col, created_at, owner_id, project_id ) VALUES ('Create Kanban', 'In Progress', '2022-07-06', 2, 2);
 INSERT INTO tasks ( name, col, created_at, owner_id, project_id ) VALUES ('Choose tech stack', 'In Progress', '2022-07-06', 3, 2);
 INSERT INTO tasks ( name, col, created_at, owner_id, project_id ) VALUES ('Create Schedule', 'Complete', '2022-07-06', 2, 2);
 INSERT INTO tasks ( name, col, created_at, owner_id, project_id ) VALUES ('Choose Idea', 'Complete', '2022-07-05', 3, 2);
