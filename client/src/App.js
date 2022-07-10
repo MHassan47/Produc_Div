d28effe0ea4a8fc0204a63a89f7d7001d9f51f5d
Mohamed <mhassankd35@gmail.com>
2022-07-10 17:21:52 -0500

    add some react routes to different components

--- client/src/App.js
+++ client/src/App.js
@@ -2,13 +2,23 @@ import { useState } from "react";
 import "./App.css";
 import Kanban from "./components//Kanban/Kanban";
 import useApplicationData from "./hooks/useApplicationData";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
   const { state, setState } = useApplicationData();
   return (
     <div className="App">
-      <div>Hello World</div>
-      <Kanban state={state} setState={setState} />
+      <BrowserRouter>
+        <Routes>
+          <Route
+            path="/dashboard"
+            element={<Kanban state={state} setState={setState} />}
+          />
+          {/* <Route path="/" element={<Homepage />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/signin" element={<SignIn />} /> */}
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
