79a3e90e83fefc9fb33b54e1e74bc9148cbbaabc
Mohamed <mhassankd35@gmail.com>
2022-07-10 19:01:32 -0500

    adds options button and styling

--- client/src/components/Task/Card.js
+++ client/src/components/Task/Card.js
@@ -1,7 +1,17 @@
 import "./card.css";
+import { BsThreeDots } from "react-icons/bs";
 
 const Card = (props) => {
-  return <div className="card">{props.children}</div>;
+  return (
+    <div className="card-container">
+      <div className="card">
+        {props.children}
+        <div className="card__option">
+          <BsThreeDots onClick={() => console.log(props.children)} />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Card;
