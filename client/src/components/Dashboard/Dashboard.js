6e9358e3e3815209f96bec1bf6228de75fa8cf85
Mohamed <mhassankd35@gmail.com>
2022-07-12 23:09:37 -0500

    creates a dashboard component that includes other components

---
+++ client/src/components/Dashboard/Dashboard.js
@@ -0,0 +1,24 @@
+import Kanban from "../Kanban/Kanban";
+import SideBar from "../SideBar/SideBar";
+import Header from "../Header/Header";
+import "./Dashboard.css";
+
+const Dashboard = ({ state, setState }) => {
+  return (
+    <div className="dashboard_container">
+      <div className="dashboard_header">
+        <Header />
+      </div>
+      <div className="dashboard_content">
+        <div className="dashboard_sidebar">
+          <SideBar />
+        </div>
+        <div className="dashboard_kanban">
+          <Kanban state={state} setState={setState} />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Dashboard;
