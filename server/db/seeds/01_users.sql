84246e85060d4fa26aa6df5c9b4d19fca817af08
Mohamed <mhassankd35@gmail.com>
2022-07-13 05:01:44 -0500

    alters passwords to be stored as hashed

--- server/db/seeds/01_users.sql
+++ server/db/seeds/01_users.sql
@@ -1,6 +1,6 @@
-INSERT INTO users (first_name, last_name, email, password, photo_url, role) VALUES ('John', 'Smith', 'johnsmith@gmail.com', 'password', 'https://i.imgur.com/twYrpay.jpg', 'Front End Developer');
-INSERT INTO users (first_name, last_name, email, password, photo_url, role) VALUES ('Jane', 'Doe', 'janedoe@gmail.com', '$2a$10$ZSN27SEQSBgC112fygR9P.Sdtlk7tub6gNaYqDiKRk/kw2tmh/Hwe', 'https://i.imgur.com/LpaY82x.png', 'Full Stack Developer');
-INSERT INTO users (first_name, last_name, email, password, photo_url, role) VALUES('Joe', 'Scott', 'joescott@gmail.com', '$2a$10$ZSN27SEQSBgC112fygR9P.Sdtlk7tub6gNaYqDiKRk/kw2tmh/Hwe', 'https://i.imgur.com/3tVgsra.jpg', 'Back End Developer');
+INSERT INTO users (first_name, last_name, email, password, photo_url, role) VALUES ('John', 'Smith', 'johnsmith@gmail.com', '$2b$10$7D9xfgBxNF/apHbHluzqxObvBM8CNMB9m3C8Zm1k2BF2VQyokkzEq', 'https://i.imgur.com/twYrpay.jpg', 'Front End Developer');
+INSERT INTO users (first_name, last_name, email, password, photo_url, role) VALUES ('Jane', 'Doe', 'janedoe@gmail.com', '$2b$10$7D9xfgBxNF/apHbHluzqxObvBM8CNMB9m3C8Zm1k2BF2VQyokkzEq', 'https://i.imgur.com/LpaY82x.png', 'Full Stack Developer');
+INSERT INTO users (first_name, last_name, email, password, photo_url, role) VALUES('Joe', 'Scott', 'joescott@gmail.com', '$2b$10$7D9xfgBxNF/apHbHluzqxObvBM8CNMB9m3C8Zm1k2BF2VQyokkzEq', 'https://i.imgur.com/3tVgsra.jpg', 'Back End Developer');
 
 
 
