37cd7458760361363ffce7643b8486a4ac0234fe
Mohamed <mhassankd35@gmail.com>
2022-07-12 23:52:08 -0500

    builds dashboard layout prototype

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -1,42 +1,38 @@
-import React from 'react';
+import React from "react";
 // import PropTypes from 'prop-types';
 // import { Button } from './Button';
-import './Header.css';
-import { Navigate, Link } from 'react-router-dom';
+import "./Header.css";
+import { Navigate, Link } from "react-router-dom";
 // import useApplicationData from "/Users/ameraalleyne/lighthouse/final/client/src/hooks/useApplicationData.js";
 // import { useState } from "react";
 
-
-
 export default function Header(props) {
- 
-    return (
-      <header className="Header">
+  return (
+    <header className="header">
+      <div>
+        <div className="header_title">
+          <h1>Project Name</h1>
+        </div>
         <div>
-          <div>
-            <h1>Project Name</h1>
-          </div>
-          <div>
-            {/* <button size="small" label="Log out" />
+          {/* <button size="small" label="Log out" />
             <>
               <button size="small" label="Log in" />
               <Link to="/register" >
               <button size="small" onClick={"/register"} label="Sign up" />
               </Link>
             </> */}
-          </div>
         </div>
-      </header>
-    );
-  }
-  // }
-  // Header.propTypes = {
-  //   user: PropTypes.shape({}),
-  //   onLogin: PropTypes.func.isRequired,
-  //   onLogout: PropTypes.func.isRequired,
-  //   onCreateAccount: PropTypes.func.isRequired,
-  // };
+      </div>
+    </header>
+  );
+}
+// }
+// Header.propTypes = {
+//   user: PropTypes.shape({}),
+//   onLogin: PropTypes.func.isRequired,
+//   onLogout: PropTypes.func.isRequired,
+//   onCreateAccount: PropTypes.func.isRequired,
+// };
 
-  // Header.defaultProps = {
-  //   user: null,  onClick={logout}   onClick={login}
-
+// Header.defaultProps = {
+//   user: null,  onClick={logout}   onClick={login}
