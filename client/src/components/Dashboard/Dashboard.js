4fe600bd0989969e21bcb1d6197264b38e468a81
Mohamed <mhassankd35@gmail.com>
2022-07-15 23:55:17 -0500

    passes the right props to components

--- client/src/components/Dashboard/Dashboard.js
+++ client/src/components/Dashboard/Dashboard.js
@@ -2,23 +2,33 @@ import Kanban from "../Kanban/Kanban";
 import SideBar from "../SideBar/SideBar";
 import Header from "../Header/Header";
 import "./Dashboard.css";
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { AuthContext } from "../../context/AuthProvider";
-import { NavLink } from "react-router-dom";
+import { NavLink, useSearchParams } from "react-router-dom";
 
 const Dashboard = ({ state, setState }) => {
+  const [currentProject, setCurrentProject] = useState(2);
   const { user } = useContext(AuthContext);
   return user ? (
     <div className="dashboard_container">
       <div className="dashboard_header">
-        <Header state={state} />
+        <Header
+          state={state}
+          currentProject={currentProject}
+          setCurrentProject={setCurrentProject}
+        />
       </div>
       <div className="dashboard_content">
         <div className="dashboard_sidebar">
           <SideBar />
         </div>
         <div className="dashboard_kanban">
-          <Kanban state={state} setState={setState} />
+          <Kanban
+            state={state}
+            setState={setState}
+            currentProject={currentProject}
+            setCurrentProject={setCurrentProject}
+          />
         </div>
       </div>
     </div>
