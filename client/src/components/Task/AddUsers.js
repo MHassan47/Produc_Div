4102b5f9e6f37011dd1a7d8d61c2170483b5c239
Mohamed <mhassankd35@gmail.com>
2022-07-17 22:35:51 -0500

    fixes conditional rendering logic

--- client/src/components/Task/AddUsers.js
+++ client/src/components/Task/AddUsers.js
@@ -1,57 +1,38 @@
-import { useContext, useEffect, useState } from "react";
+import { useContext, useState } from "react";
 import { TiUserAdd } from "react-icons/ti";
 
 import { HiCheck } from "react-icons/hi";
-import axios from "axios";
+
 import { AuthContext } from "../../context/AuthProvider";
-import PhotoUrl from "./PhotoUrl";
 
 const AddUsers = ({
   renderListContainer,
   taskID,
   state,
   setState,
-  setTest,
+  setUpdate,
   addUserToCard,
 }) => {
   const { user } = useContext(AuthContext);
   const [add, setAdd] = useState(false);
   const [checked, setChecked] = useState(false);
-  // const [test, setTest] = useState(false);
-
-  // useEffect(() =>
 
   const handleCheckClick = (e) => {
     e.preventDefault();
 
-    if (!state.users_to_tasks[taskID - 1].assigned_users.includes(user.id))
-      Promise.resolve(addUserToCard(user.id, taskID))
-        .then(() =>
-          setTimeout(() => {
-            setAdd(false);
-            setTest((prev) => !prev);
-            renderListContainer();
-            console.log("DONE");
-          }, 5000)
-        )
-        // .then(setAdd(false), setTest(true), renderListContainer())
-        .catch((error) => console.log(error));
+    // if (!state.users_to_tasks[taskID - 1].assigned_users.includes(user.id))
+    Promise.resolve(addUserToCard(user.id, taskID))
+      .then(
+        setTimeout(() => {
+          setAdd(false);
+          setUpdate((prev) => !prev);
+          renderListContainer();
+          console.log("DONE");
+        }, 50)
+      )
 
-    // .then((response) => {
-    //   console.log("===============", response);
-    //   setState({
-    //     ...state,
-    //     users_to_tasks: [...state.users_to_tasks, response.data],
-    //   }) //get list of users from new users setPhotoListContainer(newListOfUsers)
-    //     .then(() => setAdd(false), setTest(true), renderListContainer());
-    // })
+      .catch((error) => console.log(error));
   };
-  // useEffect(() => {
-  // }, [state]);
-  // useEffect(() => {
-  //   console.log("RAN");
-  //   renderListContainer();
-  // }, [test]);
 
   const handleAddClick = (e) => {
     e.preventDefault();
@@ -61,27 +42,27 @@ const AddUsers = ({
       setAdd(false);
     }, 5000);
   };
-
-  // if (!state.users_to_tasks[taskID - 1].assigned_users.includes(user.id))
-  return (
-    <>
-      {add && !checked && (
-        <div className="card_add_members">
-          <HiCheck
-            onClick={handleCheckClick}
-            className="card_add_members_icon"
-          />
-        </div>
-      )}
-      {!add && !checked && (
-        <div className="card_add_members">
-          <TiUserAdd
-            onClick={handleAddClick}
-            className="card_add_members_icon"
-          />
-        </div>
-      )}
-    </>
-  );
+  if (state.users_to_tasks[taskID - 1])
+    if (!state.users_to_tasks[taskID - 1].assigned_users.includes(user.id))
+      return (
+        <>
+          {add && !checked && (
+            <div className="card_add_members">
+              <HiCheck
+                onClick={handleCheckClick}
+                className="card_add_members_icon"
+              />
+            </div>
+          )}
+          {!add && !checked && (
+            <div className="card_add_members">
+              <TiUserAdd
+                onClick={handleAddClick}
+                className="card_add_members_icon"
+              />
+            </div>
+          )}
+        </>
+      );
 };
 export default AddUsers;
