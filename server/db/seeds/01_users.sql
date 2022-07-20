68a78a6b9ebfbad84567327a36c216a8c5aff49f
Mohamed <mhassankd35@gmail.com>
2022-07-20 15:42:43 -0500

    changes user's info

--- server/db/seeds/01_users.sql
+++ server/db/seeds/01_users.sql
@@ -1,6 +1,6 @@
-INSERT INTO users (first_name, last_name, email, password, photo_url, role) VALUES ('John', 'Smith', 'johnsmith@gmail.com', '$2b$10$7D9xfgBxNF/apHbHluzqxObvBM8CNMB9m3C8Zm1k2BF2VQyokkzEq', 'https://i.imgur.com/twYrpay.jpg', 'Front End Developer');
-INSERT INTO users (first_name, last_name, email, password, photo_url, role) VALUES ('Jane', 'Doe', 'janedoe@gmail.com', '$2b$10$7D9xfgBxNF/apHbHluzqxObvBM8CNMB9m3C8Zm1k2BF2VQyokkzEq', 'https://i.imgur.com/LpaY82x.png', 'Full Stack Developer');
-INSERT INTO users (first_name, last_name, email, password, photo_url, role) VALUES('Joe', 'Scott', 'joescott@gmail.com', '$2b$10$7D9xfgBxNF/apHbHluzqxObvBM8CNMB9m3C8Zm1k2BF2VQyokkzEq', 'https://i.imgur.com/3tVgsra.jpg', 'Back End Developer');
+INSERT INTO users (first_name, last_name, email, password, photo_url, role) VALUES ('Amera', 'A', 'Amera@A.com', '$2b$10$7D9xfgBxNF/apHbHluzqxObvBM8CNMB9m3C8Zm1k2BF2VQyokkzEq', ' https://avatars.githubusercontent.com/u/98993552?v=4', 'Full Stack Developer');
+INSERT INTO users (first_name, last_name, email, password, photo_url, role) VALUES ('Mohamed', 'H', 'Mohamed@H.com', '$2b$10$7D9xfgBxNF/apHbHluzqxObvBM8CNMB9m3C8Zm1k2BF2VQyokkzEq', 'https://avatars.githubusercontent.com/u/98605431?v=4 ', 'Full Stack Developer');
+INSERT INTO users (first_name, last_name, email, password, photo_url, role) VALUES('Sydanny', 'M', 'Sydany@M.com', '$2b$10$7D9xfgBxNF/apHbHluzqxObvBM8CNMB9m3C8Zm1k2BF2VQyokkzEq', 'https://avatars.githubusercontent.com/u/98813255?v=4', 'Full Stack Developer');
 
 
 
