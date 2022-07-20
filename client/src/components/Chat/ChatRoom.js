1002622730c7695706c7c9d77331f4c07e613380
SydannyOpal <sydanny15@gmail.com>
2022-07-20 16:18:37 -0400

    chat styling

--- client/src/components/Chat/ChatRoom.js
+++ client/src/components/Chat/ChatRoom.js
@@ -1,37 +1,31 @@
 import React, { useState, useContext } from "react";
 import ScrollToBottom from "react-scroll-to-bottom";
 import SideBar from "../SideBar/SideBar";
+import Header from "../Header/Header";
 import useChatSocket from "../../hooks/useChatSocket";
 import { AuthContext } from "../../context/AuthProvider";
 
-export default function ChatRoom({
-  state,
-  socket,
-  username,
-  room,
-  joinRoom,
-}) {
+export default function ChatRoom({ state, socket, username, room, joinRoom }) {
+  const [currentProject, setCurrentProject] = useState(1);
   const [currentMessage, setCurrentMessage] = useState("");
-  const {user} = useContext(AuthContext)
+  const { user } = useContext(AuthContext);
   const { sendChatMessage, chatMessages } = useChatSocket(user.first_name);
   const sendMessage = () => {
     sendChatMessage(currentMessage);
     setCurrentMessage("");
   };
 
-  const [currentProject, setCurrentProject] = useState(1);
-
-
   console.log("Chat messages: ", chatMessages);
   return (
     <div className="chat_body">
       <div className="chat_container">
-        <Header 
-        state={state} 
-      currentProject={currentProject}
-      setCurrentProject={setCurrentProject}
-        />
-
+        <div className="chat_header">
+          <Header
+            state={state}
+            currentProject={currentProject}
+            setCurrentProject={setCurrentProject}
+          />
+        </div>
         <div className="chat_content">
           <div>
             <SideBar />
@@ -41,9 +35,9 @@ export default function ChatRoom({
               {chatMessages?.map(({ message, username, timestamp }) => (
                 <div key={message + username} className="message-content">
                   <p>
-                    <small className="time-stamp">[{timestamp}] </small>
-                    <strong>{username}:</strong>
-                    {message}{" "}
+                    <small className="time-stamp">{timestamp} </small>
+                    <strong className="username"> {username}: </strong>
+                    <normal className="message"> {message}</normal>
                   </p>
                 </div>
               ))}
@@ -64,7 +58,7 @@ export default function ChatRoom({
                   }}
                 />
                 <button className="send-chat" onClick={sendMessage}>
-                  &#9658;
+                  Send
                 </button>
               </div>
             </div>
--- client/src/settings.js
+++ client/src/settings.js
@@ -2,8 +2,8 @@ import { createClient, createMicrophoneAndCameraTracks } from "agora-rtc-react";
 
 const appId = "c3accc8c4273406c80f00850cb0706ae";
 // every day create a new temp token
-const token = "006c3accc8c4273406c80f00850cb0706aeIAB+DsKcD/prRPlxVSjT2ea2om/hCdFYCkCnLV/d4UQPqxgwSN4AAAAAEACPl0pWDA7WYgEAAQAMDtZi"
-
+const token =
+  "006c3accc8c4273406c80f00850cb0706aeIADDnZkxqcmeCcQ3NpGyED32wQhEQZ/tt5rOFLGpOsgr6hgwSN4AAAAAEAAC5FBkMH/ZYgEAAQAwf9li";
 // rtc - real-time communication
 export const config = { mode: "rtc", codec: "vp8", appId, token };
 export const useClient = createClient(config);
