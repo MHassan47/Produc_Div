6aa3d22aacf9a6752a183421c6db35f25c3b0062
Mohamed <mhassankd35@gmail.com>
2022-07-19 21:24:32 -0500

    refactors user state

--- client/src/components/Auth/SignIn.js
+++ client/src/components/Auth/SignIn.js
@@ -2,57 +2,53 @@ import axios from "axios";
 import { useRef, useState, useEffect, useContext, React } from "react";
 import { useNavigate } from "react-router-dom";
 import { NavLink } from "react-router-dom";
-import "./SignIn.css"
+import "./SignIn.css";
 import { AuthContext } from "../../context/AuthProvider";
-import { makeStyles } from '@material-ui/core/styles';
-import { AppBar, IconButton, Toolbar, Collapse } from '@material-ui/core';
-import Typical from 'react-typical'
-
+import { makeStyles } from "@material-ui/core/styles";
+import { AppBar, IconButton, Toolbar, Collapse } from "@material-ui/core";
+import Typical from "react-typical";
 
 const useStyles = makeStyles((theme) => ({
   root: {
-    display: 'flex',
-    justifyContent: 'center',
-    alignItems: 'center',
-    height: '100vh',
-    fontFamily: 'monospace',
-    fontSize: '1.2em',
-    background: "#000"
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    height: "100vh",
+    fontFamily: "monospace",
+    fontSize: "1.2em",
+    background: "#000",
   },
   appbar: {
-    background: '#323132',
+    background: "#323132",
   },
   appbarWrapper: {
-    width: '80%',
-    margin: '0 auto',
+    width: "80%",
+    margin: "0 auto",
   },
   appbarTitle: {
-    flexGrow: '1',
+    flexGrow: "1",
   },
   icon: {
-    color: '#fff',
-    fontSize: '2rem',
+    color: "#fff",
+    fontSize: "2rem",
   },
   colorText: {
-    fontFamily: 'monospace',
-    fontSize: '2.5rem',
-    color: '#a425ff',
+    fontFamily: "monospace",
+    fontSize: "2.5rem",
+    color: "#a425ff",
   },
   container: {
-    textAlign: 'center',
+    textAlign: "center",
   },
   title: {
-    color: '#000',
-    fontSize: '4.5rem',
+    color: "#000",
+    fontSize: "4.5rem",
   },
   goDown: {
-    color: '#fff',
-    fontSize: '4rem',
-  }
+    color: "#fff",
+    fontSize: "4rem",
+  },
 }));
-
-
-
 
 export default function SignIn({ state, setState }) {
   const classes = useStyles();
@@ -91,81 +87,81 @@ export default function SignIn({ state, setState }) {
   const handleSubmit = (event) => {
     event.preventDefault();
 
-    if (validateLogin())
-      console.log("**************SUBMIT Sign-In clicked **************");
+    // console.log("**************SUBMIT Sign-In clicked **************");
     const body = {
       email,
       password,
     };
-    axios
-      .post("http://localhost:8080/users/sign-in", body, {
-        withCredentials: true,
-      })
-      // const userArr = [];
+    if (validateLogin())
+      axios
+        .post("http://localhost:8080/users/sign-in", body, {
+          withCredentials: true,
+        })
+        // const userArr = [];
 
-      .then(
-        ({ data }) => {
-          localStorage.setItem("user", JSON.stringify(data.user))
-          // console.log("//////DATA//////", data)
-          // userStateArray.map((user) => {
-          //   if (user.email === email) {
-          //     console.log("##########", user.id);
-          console.log("//////DATA//////", data.user)
+        .then(
+          ({ data }) => {
+            // localStorage.setItem("user", JSON.stringify(data.user))
+            // console.log("//////DATA//////", data)
+            // userStateArray.map((user) => {
+            //   if (user.email === email) {
+            //     console.log("##########", user.id);
+            // console.log("//////DATA//////", data.user);
 
-          return login(data.user);
-        }
-        //   });
-      )
-      .then(() => navigate("/dashboard", { replace: true }))
-      .then(() => console.log("THIS IS STATE", state))
+            return login(data.user);
+          }
+          //   });
+        )
+        .then(() => navigate("/dashboard", { replace: true }))
+        // .then(() => console.log("THIS IS STATE", state))
 
-      .catch((error) => {
-        setErrMsg(error.response.data.error);
-      });
+        .catch((error) => {
+          setErrMsg(error.response.data.error);
+        });
   };
 
   return (
-
     <div className="background-colour">
       <AppBar className={classes.appbar} elevation={0}>
         <Toolbar className={classes.appbarWrapper}>
           <h1 className={classes.appbarTitle}>
-            <span href="/Homepage" className={classes.colorText}>Produc_Div</span>
+            <span href="/Homepage" className={classes.colorText}>
+              Produc_Div
+            </span>
           </h1>
-          <NavLink to='/sign-in' >
-            <button className='buttons'>Sign In</button>
+          <NavLink to="/sign-in">
+            <button className="buttons">Sign In</button>
           </NavLink>
-          <NavLink to='/register'> <button className='buttons'>Register</button></NavLink>
+          <NavLink to="/register">
+            {" "}
+            <button className="buttons">Register</button>
+          </NavLink>
           {/* 
           <IconButton>
             <SortIcon className={classes.icon} />
           </IconButton> */}
-
         </Toolbar>
       </AppBar>
-      <section className="section"  >
+      <section className="section">
         <div className="sign-in-h1">
           {/* <h1>Sign In</h1> */}
           <Typical
-        loop={Infinity}
-        wrapper="l"
-        steps={[
-          ' ',
-          1000,
-          'Sign In', 
-          6000,
-        ]}
-        />
+            loop={Infinity}
+            wrapper="l"
+            steps={[" ", 1000, "Sign In", 6000]}
+          />
         </div>
-        <div >
+        <div>
           <form className="signIn_form" onSubmit={handleSubmit}>
-            <div >
-              <label className="yt-font" htmlFor="email">Email:&nbsp;&nbsp;&nbsp; </label>
+            <div>
+              <label className="yt-font" htmlFor="email">
+                Email:&nbsp;&nbsp;&nbsp;{" "}
+              </label>
               <input
                 type="text"
                 id="email"
                 fontSize="2em"
-                style={{ height: '2.5em', width: '40%'}}
+                style={{ height: "2.5em", width: "40%" }}
                 ref={userRef}
                 autoComplete="off"
                 onChange={(event) => setEmail(event.target.value)}
@@ -173,14 +169,15 @@ export default function SignIn({ state, setState }) {
                 required
               />
             </div>
-            < br />
+            <br />
             <div>
-              <label className="yt-font" htmlFor="password">Password:&nbsp;&nbsp;&nbsp;</label>
+              <label className="yt-font" htmlFor="password">
+                Password:&nbsp;&nbsp;&nbsp;
+              </label>
               <input
                 type="password"
                 id="password"
-                style={{ height: '2.5em', width: '40%'}}
-
+                style={{ height: "2.5em", width: "40%" }}
                 onChange={(event) => setPassword(event.target.value)}
                 value={password}
                 required
@@ -191,30 +188,30 @@ export default function SignIn({ state, setState }) {
               <button
                 type="submit"
                 className="btn"
-                style={{ marginLeft: '8em'}}
-              // onClick={(event) => handleSubmit()}
+                style={{ marginLeft: "8em" }}
+                // onClick={(event) => handleSubmit()}
               >
                 Sign In
               </button>
             </div>
           </form>
 
-            <p className="footer">
-              Don't have an account?
-                </p>
-              <br />
-              <span className="link">
-                {/* put router link here */}
-                <button
-                  type="button"
-                  className="btn"
-                  style={{ marginLeft: '18em'}}
-                  onClick={(event) => handleSubmit()}
-                >
-                  {" "}
-                  <a className="register-btn " href="/register">Register</a>{" "}
-                </button>
-              </span>
+          <p className="footer">Don't have an account?</p>
+          <br />
+          <span className="link">
+            {/* put router link here */}
+            <button
+              type="button"
+              className="btn"
+              style={{ marginLeft: "18em" }}
+              onClick={(event) => handleSubmit()}
+            >
+              {" "}
+              <a className="register-btn " href="/register">
+                Register
+              </a>{" "}
+            </button>
+          </span>
         </div>
       </section>
     </div>
