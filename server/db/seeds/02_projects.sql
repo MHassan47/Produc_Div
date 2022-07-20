a5016fc1383dba27e9635d28d9a7a648fe003fdf
Mohamed <mhassankd35@gmail.com>
2022-07-20 15:44:07 -0500

    changes tasks and projects info in db

--- server/db/seeds/02_projects.sql
+++ server/db/seeds/02_projects.sql
@@ -1,6 +1,5 @@
-INSERT INTO projects (name, owner_id) VALUES ('Final Project', 1);
-INSERT INTO projects (name, owner_id) VALUES ('test2', 2);
-INSERT INTO projects (name, owner_id) VALUES ('test2', 2);
+INSERT INTO projects (name, owner_id) VALUES ('LHL - Final Project', 1);
+INSERT INTO projects (name, owner_id) VALUES ('New Project', 2);
 
 INSERT INTO users_to_projects (user_id, project_id) VALUES (2, 1);
 INSERT INTO users_to_projects (user_id, project_id) VALUES (3, 1);
--- server/db/seeds/03_tasks.sql
+++ server/db/seeds/03_tasks.sql
@@ -1,40 +1,39 @@
 
-INSERT INTO tasks ( name, col, created_at, owner_id, project_id ) VALUES ('Layout Design', 'To Do', '2022-07-07', 1, 1);
-INSERT INTO tasks ( name, col, created_at, owner_id, project_id ) VALUES ('Database Design', 'To Do', '2022-07-08', 1, 1);
-INSERT INTO tasks ( name, col, created_at, owner_id, project_id ) VALUES ('Create kanban', 'In Progress', '2022-07-06', 2, 1);
-INSERT INTO tasks ( name, col, created_at, owner_id, project_id ) VALUES ('Choose tech stack', 'In Progress', '2022-07-06', 3, 1);
-INSERT INTO tasks ( name, col, created_at, owner_id, project_id ) VALUES ('Create Schedule', 'Complete', '2022-07-06', 2, 1);
-INSERT INTO tasks ( name, col, created_at, owner_id, project_id ) VALUES ('Choose Idea', 'Complete', '2022-07-05', 3, 1);
+INSERT INTO tasks ( name, col, created_at, owner_id, project_id ) VALUES ('User can create more kanban columns', 'To Do', '2022-07-08', 2, 1);
+INSERT INTO tasks ( name, col, created_at, owner_id, project_id ) VALUES ('User can send images in chat', 'To Do', '2022-07-06', 2, 1);
+INSERT INTO tasks ( name, col, created_at, owner_id, project_id ) VALUES ('User can create personal workspace', 'To Do', '2022-07-06', 2, 1);
+INSERT INTO tasks ( name, col, created_at, owner_id, project_id ) VALUES ('User can share screen in conference', 'To Do', '2022-07-05', 3, 1);
+INSERT INTO tasks ( name, col, created_at, owner_id, project_id ) VALUES ('Project README', 'In Progress', '2022-07-07', 1, 1);
+INSERT INTO tasks ( name, col, created_at, owner_id, project_id ) VALUES ('Responsive Design', 'In Progress', '2022-07-05', 3, 1);
+INSERT INTO tasks ( name, col, created_at, owner_id, project_id ) VALUES ('Users can video chat in conference', 'Complete', '2022-07-05', 3, 1);
+INSERT INTO tasks ( name, col, created_at, owner_id, project_id ) VALUES ('Users can live message in chat', 'Complete', '2022-07-05', 3, 1);
+INSERT INTO tasks ( name, col, created_at, owner_id, project_id ) VALUES ('Users can drag cards to different columns ', 'Complete', '2022-07-05', 3, 1);
+INSERT INTO tasks ( name, col, created_at, owner_id, project_id ) VALUES ('Choose tech stack', 'Complete', '2022-07-06', 3, 1);
+INSERT INTO tasks ( name, col, created_at, owner_id, project_id ) VALUES ('User can creat and edit card', 'Complete', '2022-07-05', 3, 1);
 
-INSERT INTO tasks ( name, col, created_at, owner_id, project_id ) VALUES ('Layout Design', 'To Do', '2022-07-07', 1, 2);
-INSERT INTO tasks ( name, col, created_at, owner_id, project_id ) VALUES ('Database Design', 'To Do', '2022-07-08', 1, 2);
-INSERT INTO tasks ( name, col, created_at, owner_id, project_id ) VALUES ('Create Kanban', 'In Progress', '2022-07-06', 2, 2);
-INSERT INTO tasks ( name, col, created_at, owner_id, project_id ) VALUES ('Choose tech stack', 'In Progress', '2022-07-06', 3, 2);
-INSERT INTO tasks ( name, col, created_at, owner_id, project_id ) VALUES ('Create Schedule', 'Complete', '2022-07-06', 2, 2);
-INSERT INTO tasks ( name, col, created_at, owner_id, project_id ) VALUES ('Choose Idea', 'In Progress', '2022-07-05', 3, 2);
+
 
 
 INSERT INTO users_to_tasks (user_id, task_id) VALUES (1, 1);
 INSERT INTO users_to_tasks (user_id, task_id) VALUES (2, 1);
 INSERT INTO users_to_tasks (user_id, task_id) VALUES (3, 1);
-INSERT INTO users_to_tasks (user_id, task_id) VALUES (1, 2);
 INSERT INTO users_to_tasks (user_id, task_id) VALUES (3, 2);
 INSERT INTO users_to_tasks (user_id, task_id) VALUES (1, 3);
 INSERT INTO users_to_tasks (user_id, task_id) VALUES (2, 3);
+INSERT INTO users_to_tasks (user_id, task_id) VALUES (3, 3);
 INSERT INTO users_to_tasks (user_id, task_id) VALUES (1, 4);
-INSERT INTO users_to_tasks (user_id, task_id) VALUES (2, 4);
-INSERT INTO users_to_tasks (user_id, task_id) VALUES (3, 5);
 INSERT INTO users_to_tasks (user_id, task_id) VALUES (1, 5);
-INSERT INTO users_to_tasks (user_id, task_id) VALUES (3, 6);
+INSERT INTO users_to_tasks (user_id, task_id) VALUES (2, 5);
+INSERT INTO users_to_tasks (user_id, task_id) VALUES (3, 5);
+INSERT INTO users_to_tasks (user_id, task_id) VALUES (1, 6);
 INSERT INTO users_to_tasks (user_id, task_id) VALUES (2, 6);
-
 INSERT INTO users_to_tasks (user_id, task_id) VALUES (1, 7);
-INSERT INTO users_to_tasks (user_id, task_id) VALUES (2, 7);
 INSERT INTO users_to_tasks (user_id, task_id) VALUES (3, 8);
-INSERT INTO users_to_tasks (user_id, task_id) VALUES (1, 8);
-INSERT INTO users_to_tasks (user_id, task_id) VALUES (3, 9);
-INSERT INTO users_to_tasks (user_id, task_id) VALUES (1, 9);
 INSERT INTO users_to_tasks (user_id, task_id) VALUES (2, 9);
 INSERT INTO users_to_tasks (user_id, task_id) VALUES (1, 10);
+INSERT INTO users_to_tasks (user_id, task_id) VALUES (2, 10);
+INSERT INTO users_to_tasks (user_id, task_id) VALUES (3, 10);
 INSERT INTO users_to_tasks (user_id, task_id) VALUES (2, 11);
-INSERT INTO users_to_tasks (user_id, task_id) VALUES (3, 12);
+
+
+
